c52836144aca71ff28ac92882b7cef6adf92721c
Will <willokans@outlook.com>
2019-01-27 13:33:07 +0000

    work in progress on screenplay

--- src/test/java/net/serenity/practice/features/navigation/WhenBrowserAppNavigationTabs.java
+++ src/test/java/net/serenity/practice/features/navigation/WhenBrowserAppNavigationTabs.java
@@ -2,12 +2,24 @@ package net.serenity.practice.features.navigation;
 
 import net.serenity.practice.Steps.NavigatingUser;
 import net.serenity.practice.model.Tabs;
+//import net.serenity.practice.tasks.NavigateTo;
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Manual;
 import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+
+import static net.serenity.practice.model.Tabs.Opportunities;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static net.thucydides.core.annotations.ClearCookiesPolicy.BeforeEachTest;
+import static org.hamcrest.Matchers.containsString;
 
 
 @RunWith(SerenityRunner.class)
@@ -16,7 +28,7 @@ public class WhenBrowserAppNavigationTabs {
     @Steps
     NavigatingUser barry;
 
-    @Managed
+    @Managed (driver = "chrome", uniqueSession=true, clearCookies=BeforeEachTest)
     WebDriver browser;
 
     @Test
@@ -25,11 +37,23 @@ public class WhenBrowserAppNavigationTabs {
         barry.isOnTheHomePage();
 
         //When
-        barry.navigatesToOpportunityPage(Tabs.Opportunities);
+        barry.navigatesToOpportunityPage(Opportunities);
 
         //Then
         barry.shouldSeePageTitleContaining("Opportunities");
 
+    }
+
+    @Manual
+    @Test
+    public void shouldBeAbleToViewAllAccountsList(){
+
+        Actor will = Actor.named("Will");
+        will.can(BrowseTheWeb.with(browser));
+
+//        when(will).attemptsTo(NavigateTo.theApplication(Opportunities));
+
+        then(will).should(seeThat(TheWebPage.title(), containsString("Opportunities")));
     }
 
 }
---
+++ src/test/java/net/serenity/practice/ui/NavigateBar.java
@@ -0,0 +1,12 @@
+package net.serenity.practice.ui;
+
+import net.serenity.practice.model.Tabs;
+
+import java.lang.annotation.Target;
+
+//public class NavigateBar {
+//    public static Target tabs(Tabs tabs){
+////        return Target.the(tabs.name() + " oppotuniry")
+////                .locatedBy()
+////    }
+//}
